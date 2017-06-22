d867f121e6c953b5b954c05e2a80fbe7d1b960cc
Daniel Bonaparte <abonaparte3@yahoo.com>
2017-06-22 16:55:55 -0400

    Fixed Database functionality issue. Specs met. Working on validation issues.

--- src/main/java/database/example/demo/MainController.java
+++ src/main/java/database/example/demo/MainController.java
@@ -1,9 +1,12 @@
 package database.example.demo;
 
+import javax.validation.Valid;
+
 import org.hibernate.sql.Template;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -30,18 +33,32 @@ public class MainController {
 		//userModel.addAttribute(new User());
 		// @ResponseBody means the returned String is the response, not a view name
 		// @RequestParam means it is a parameter from the GET or POST request
-		User n = new User();
+		
+		/*User n = new User();
 		n.setName(name);
 		n.setFood(food);
-		userRepository.save(n);
+		userRepository.save(n);*/
 		return "list";
 	}
-	
+	/*THe below is for passing the object WITHOUT validation */
+	/*
 	@PostMapping("/list")
 	public String usersAdded(@ModelAttribute User user){
+		userRepository.save(user);
 		return "result";
 	}
+	/***********************************************************/
+	
+	
+	@PostMapping("/list")
+	public String usersAdded(@Valid User user, BindingResult bindingResult){
 		
+		if (bindingResult.hasErrors()){
+			return "list";
+		}
+		userRepository.save(user);
+		return "result";
+	}
 
 	@GetMapping(path="/all")
 	public @ResponseBody Iterable<User> getAllUsers() {
--- src/main/java/database/example/demo/User.java
+++ src/main/java/database/example/demo/User.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
@@ -11,8 +13,12 @@ public class User {
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
+    @NotNull
+    @Size(min=3, max=30)
     private String name;
     
+    @NotNull
+    @Size(min=3, max=30)
     private String food;
 
     //private String email;

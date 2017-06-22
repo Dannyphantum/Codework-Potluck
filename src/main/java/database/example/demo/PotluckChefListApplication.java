f578f11c884b11678e91b07a85735a76c3fa0321
Daniel Bonaparte <abonaparte3@yahoo.com>
2017-06-22 16:14:06 -0400

    First commit. App partially functional; requires update to post to database.

---
+++ src/main/java/database/example/demo/PotluckChefListApplication.java
@@ -0,0 +1,12 @@
+package database.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotluckChefListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotluckChefListApplication.class, args);
+	}
+}
---
+++ src/test/java/database/example/demo/PotluckChefListApplicationTests.java
@@ -0,0 +1,16 @@
+package database.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PotluckChefListApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
